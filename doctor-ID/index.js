c74d27f58f2c469f9c0303af7cdb12c7f4a5683c
khushboo8252 <khushbooranjan8252@gmail.com>
2023-09-05 17:45:17 +0530

    addded doctor_ID

--- doctor-ID/index.js
+++ doctor-ID/index.js
@@ -1,3 +1,5 @@
+// fill in javascript code here
+
 
 
 // fill in javascript code here
@@ -5,12 +7,12 @@
 document.addEventListener("DOMContentLoaded", function () {
     let myForm = document.querySelector("form");
     let inpName = document.getElementById("name");
-    let inpEmployee = document.getElementById("employeeID");
-    let inpDepartment = document.getElementById("department");
+    let inpDoc = document.getElementById("docID");
+    let inpDept = document.getElementById("dept");
     let inpExp = document.getElementById("exp");
     let inpEmail = document.getElementById("email");
     let inpMbl = document.getElementById("mbl");
-    let tbody = document.getElementById("employeeTableBody");
+    let tbody = document.getElementById("docTableBody");
   
     myForm.addEventListener("submit", function (e) {
       e.preventDefault();
@@ -22,19 +24,20 @@ document.addEventListener("DOMContentLoaded", function () {
       } else if (experience >= 2 && experience <= 5) {
         role = "Junior";
       } else {
-        role = "Fresher";
+        role = "Trainee";
       }
   
       let Data = {
         name: inpName.value,
-        employeeID: inpEmployee.value,
-        department: inpDepartment.value,
+        docID: inpDoc.value,
+        dept: inpDept.value,
         exp: experience, // Store the numeric value of experience
         email: inpEmail.value,
         mbl: inpMbl.value,
         role: role,
       };
   
+      // Reset the form fields
       myForm.reset();
   
       let tr = document.createElement("tr");
@@ -48,8 +51,8 @@ document.addEventListener("DOMContentLoaded", function () {
       let td8 = document.createElement("td");
   
       td1.innerText = Data.name;
-      td2.innerText = Data.employeeID;
-      td3.innerText = Data.department;
+      td2.innerText = Data.docID;
+      td3.innerText = Data.dept;
       td4.innerText = Data.exp;
       td5.innerText = Data.email;
       td6.innerText = Data.mbl;
@@ -67,4 +70,5 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
   
+
 
